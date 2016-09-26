55131d7d491462a8b0a6f480cdf75d95053dfd3a
Robert Williams <rjwil35@verizon.net>
2016-09-26 12:53:33 -0400

    initial project

---
+++ src/main/java/com/bobWilliams/ActiveMQ_CleanCode_Chp3/App.java
@@ -0,0 +1,13 @@
+package com.bobWilliams.ActiveMQ_CleanCode_Chp3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
